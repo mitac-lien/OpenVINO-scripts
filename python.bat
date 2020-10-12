f55df7753387793557b867f5c193f5f4108a3c95
mitac-lien <c.h.lien@mic.com.tw>
2020-10-12 17:03:29 +0800

    Add python.bat

---
+++ python.bat
@@ -0,0 +1,8 @@
+﻿@echo off
+
+set "OPENVINO_DIR=C:\Program Files (x86)\IntelSWTools\openvino_2020.4.287"
+call "%OPENVINO_DIR%\bin\setupvars.bat"
+
+set "PYTHON=%LOCALAPPDATA%\Programs\Python\Python37\python.exe"
+
+"%PYTHON%"
