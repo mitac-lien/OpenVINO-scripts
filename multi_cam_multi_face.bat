337db5817ade60d11e08da7fa0f2c4d3753bcfe5
mitac-lien <c.h.lien@mic.com.tw>
2020-10-19 15:21:21 +0800

    Update parameters

--- multi_cam_multi_face.bat
+++ multi_cam_multi_face.bat
@@ -7,7 +7,7 @@ set "PYTHON=%LOCALAPPDATA%\Programs\Python\Python37\python.exe"
 
 set "INTEL_MODEL_DIR=%OPENVINO_DIR%\models\intel"
 set "MODEL_DETECTOR=%INTEL_MODEL_DIR%\face-detection-retail-0005\FP16\face-detection-retail-0005.xml"
-set "MODEL_REID=%INTEL_MODEL_DIR%\person-reidentification-retail-0270\FP16\person-reidentification-retail-0270.xml"
+set "MODEL_AGE_GENDER=%INTEL_MODEL_DIR%\age-gender-recognition-retail-0013\FP16\age-gender-recognition-retail-0013.xml"
 set "PROJECT_DIR=%USERPROFILE%\Desktop\multi_camera_multi_face_tracking"
 
 set "DEBUG=1"
@@ -21,4 +21,5 @@ cd "%PROJECT_DIR%"
 "%PYTHON%" %PY_DEBUG% main.py ^
     -i 0 0 ^
     --m_detector "%MODEL_DETECTOR%" ^
+    --m_ag "%MODEL_AGE_GENDER%" ^
     --config configs/person.py
